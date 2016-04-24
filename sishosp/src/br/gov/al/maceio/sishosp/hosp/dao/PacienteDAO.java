7c845d7631b7ee0845df4cccfb24038b6e153ee8
Thulio <Thulio@DESKTOP-VBOJMKV>
2016-04-24 16:54:58 -0300

    Alterar e Select do paciente

--- sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/PacienteDAO.java
+++ sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/PacienteDAO.java
@@ -10,8 +10,12 @@ import java.util.List;
 
 import br.gov.al.maceio.sishosp.comum.exception.ProjetoException;
 import br.gov.al.maceio.sishosp.comum.util.ConnectionFactory;
+import br.gov.al.maceio.sishosp.hosp.model.EncaminhadoBean;
 import br.gov.al.maceio.sishosp.hosp.model.EscolaBean;
+import br.gov.al.maceio.sishosp.hosp.model.EscolaridadeBean;
+import br.gov.al.maceio.sishosp.hosp.model.FormaTransporteBean;
 import br.gov.al.maceio.sishosp.hosp.model.PacienteBean;
+import br.gov.al.maceio.sishosp.hosp.model.ProfissaoBean;
 import br.gov.al.maceio.sishosp.hosp.model.RacaBean;
 
 public class PacienteDAO {
@@ -32,8 +36,8 @@ public class PacienteDAO {
 
 		String sql = "insert into hosp.pacientes (dtacadastro, nome, dtanascimento, estcivil, sexo, sangue, "
 				+ "pai, mae, conjuge,codraca, cep, uf, cidade, bairro, logradouro, numero, complemento, referencia, telres, telcel, teltrab, telorelhao, rg, oe, dtaexpedicaorg, cpf, cns, protreab, "
-				+ "reservista, ctps, serie, pis, cartorio, regnascimento, livro, folha, dtaregistro, contribuinte, codescolaridade, codescola, codprofissao, trabalha, localtrabalha, codparentesco, "
-				+ "nomeresp, rgresp, cpfresp, dtanascimentoresp, codencaminhado, codformatransporte ,deficiencia, tipodeficiencia)"
+				+ "reservista, ctps, serie, pis, cartorio, regnascimento, livro, folha, dtaregistro, contribuinte, id_escolaridade, id_escola, id_profissao, trabalha, localtrabalha, codparentesco, "
+				+ "nomeresp, rgresp, cpfresp, dtanascimentoresp, id_encaminhado, id_formatransporte ,deficiencia, tipodeficiencia)"
 				+ " values (CURRENT_TIMESTAMP, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ? , ? , ?, ? , ? , "
 				+ "? , ? , ?, ?, ?, ? , ? , ?, ? , ?, ?, ?, ? , ? , ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
@@ -167,11 +171,69 @@ public class PacienteDAO {
 
 	public Boolean alterar(PacienteBean paciente) throws ProjetoException {
 		boolean alterou = false;
-		String sql = "update hosp.pacientes set nome = ? where id_paciente = ?";
+		String sql = "update hosp.pacientes set nome = ?, set dtanascimento = ?, set estcivil = ?, set sexo = ? , set sangue = ?, set pai = ? "
+				+ ", set mae = ?, set conjuge = ?, set codraca = ?, set cep = ?, set uf = ?, set cidade = ?, set bairro = ?, set logradouro = ?, set numero = ?"
+				+ ", set complemento = ?, set referencia = ?, set telres = ?, set telcel = ?, set teltrab = ?, set telorelhao = ?"
+				+ ", set rg = ?, set oe = ?, set dtaexpedicaorg = ?, set cpf = ?, set cns = ?, set protreab = ?"
+				+ ", set reservista = ?, set ctps = ?, set serie = ?, set pis = ?, set cartorio = ?, set regnascimento = ?, set livro = ?, set folha = ?, set dtaregistro = ?"
+				+ ", set contribuinte = ?, set id_escolaridade = ?, set id_escola = ?, set id_profissao = ?, set trabalha = ?, set localtrabalha = ?"
+				+ ", set codparentesco = ?, set nomeresp = ?, set rgresp = ?, set cpfresp = ?, set dtanascimentoresp = ?, set id_encaminhado = ?"
+				+ ", set id_formatransporte = ?, set deficiencia = ?, set tipodeficiencia = ? where id_paciente = ?";
 		try {
 			conexao = ConnectionFactory.getConnection();
 			PreparedStatement stmt = conexao.prepareStatement(sql);
 			stmt.setString(1, paciente.getNome());
+			stmt.setDate(2, new java.sql.Date(paciente.getDtanascimento().getTime()));
+			stmt.setString(3, paciente.getEstadoCivil());
+			stmt.setString(4, paciente.getSexo());
+			stmt.setString(5, paciente.getSangue());
+			stmt.setString(6, paciente.getNomePai());
+			stmt.setString(7, paciente.getNomeMae());
+			stmt.setString(8, paciente.getConjuge());
+			stmt.setInt(9, paciente.getCodRaca());
+			stmt.setInt(10, paciente.getEndereco().getCep());
+			stmt.setString(11, paciente.getEndereco().getUf());
+			stmt.setString(12, paciente.getEndereco().getMunicipio());
+			stmt.setString(13, paciente.getEndereco().getBairro());
+			stmt.setString(14, paciente.getEndereco().getLogradouro());
+			stmt.setString(15, paciente.getEndereco().getNumero());
+			stmt.setString(16, paciente.getEndereco().getComplemento());
+			stmt.setString(17, paciente.getEndereco().getReferencia());
+            stmt.setString(18, paciente.getEndereco().getTelefoneres());
+            stmt.setString(19, paciente.getEndereco().getTelefonecel());
+            stmt.setString(20, paciente.getEndereco().getTelefonetrab());
+		    stmt.setString(21, paciente.getEndereco().getTelefoneorelhao());
+			stmt.setString(22, paciente.getRg());
+			stmt.setString(23, paciente.getOe());
+			stmt.setDate(24, new java.sql.Date(paciente.getDataExpedicao1().getTime()));
+			stmt.setString(25, paciente.getCpf());
+			stmt.setString(26, paciente.getCns());
+			stmt.setInt(27, paciente.getProtant());
+			stmt.setString(28, paciente.getReservista());
+			stmt.setInt(29, paciente.getCtps());
+			stmt.setInt(30, paciente.getSerie());
+			stmt.setString(31, paciente.getPis());
+			stmt.setString(32, paciente.getCartorio());
+			stmt.setString(33, paciente.getNumeroCartorio());
+			stmt.setString(34, paciente.getLivro());
+			stmt.setInt(35, paciente.getFolha());
+			stmt.setDate(36, new java.sql.Date(paciente.getDataExpedicao2().getTime()));
+			stmt.setString(37, paciente.getAssociado().toUpperCase().trim());
+            stmt.setInt(38, paciente.getEscolaridade().getCodescolaridade());
+            stmt.setInt(39, paciente.getEscola().getCodEscola());
+            stmt.setInt(40, paciente.getProfissao().getCodprofissao());
+			stmt.setString(41, paciente.getTrabalha());
+			stmt.setString(42, paciente.getLocaltrabalha());
+			stmt.setInt(43, paciente.getCodparentesco());
+			stmt.setString(44, paciente.getNomeresp());
+			stmt.setString(45, paciente.getRgresp());
+			stmt.setString(46, paciente.getCpfresp());
+			stmt.setDate(47, new java.sql.Date(paciente.getDataNascimentoresp().getTime()));
+            stmt.setInt(48, paciente.getEncaminhado().getCodencaminhado());
+            stmt.setInt(49, paciente.getFormatransporte().getCodformatransporte());
+            stmt.setString(50, paciente.getDeficiencia());
+            stmt.setString(51, paciente.getTipoDeficiencia());
+			
 			stmt.executeUpdate();
 
 			conexao.commit();
@@ -217,10 +279,22 @@ public class PacienteDAO {
 
 	  public ArrayList<PacienteBean> listaPacientes() {
 
-          String sql = "select id_paciente, nome, dtanascimento, estcivil, sexo, sangue, "
-          		+ "pai, mae, conjuge,codraca, cep, uf, cidade, bairro, logradouro, numero, complemento, referencia, telres, telcel, teltrab, telorelhao, rg, oe, dtaexpedicaorg, cpf, cns, protreab, "
-          		+ "reservista, ctps, serie, pis, cartorio, regnascimento, livro, folha, dtaregistro, contribuinte, codescolaridade, codescola, codprofissao, trabalha, localtrabalha, codparentesco, "
-          		+ "nomeresp, rgresp, cpfresp, dtanascimentoresp, codencaminhado, codformatransporte ,deficiencia, tipodeficiencia from hosp.pacientes order by nome";
+          String sql = "select pacientes.id_paciente, pacientes.nome, pacientes.dtanascimento, pacientes.estcivil, pacientes.sexo, pacientes.sangue, "
+          		+ "pacientes.pai, pacientes.mae, pacientes.conjuge,pacientes.codraca, pacientes.cep, pacientes.uf, pacientes.cidade, pacientes.bairro, "
+          		+ "pacientes.logradouro, pacientes.numero, pacientes.complemento, pacientes.referencia, pacientes.telres, pacientes.telcel,"
+          		+ " pacientes.teltrab, pacientes.telorelhao, pacientes.rg, pacientes.oe, pacientes.dtaexpedicaorg, pacientes.cpf, pacientes.cns, "
+          		+ "pacientes.protreab, "
+          		+ "pacientes.reservista, pacientes.ctps, pacientes.serie, pacientes.pis, pacientes.cartorio, pacientes.regnascimento, pacientes.livro, "
+          		+ "pacientes.folha, pacientes.dtaregistro, pacientes.contribuinte, pacientes.id_escolaridade, pacientes.id_escola, pacientes.id_profissao, "
+          		+ "pacientes.trabalha, pacientes.localtrabalha, pacientes.codparentesco, "
+          		+ "pacientes.nomeresp, pacientes.rgresp, pacientes.cpfresp, pacientes.dtanascimentoresp, pacientes.id_encaminhado, "
+          		+ "pacientes.id_formatransporte ,pacientes.deficiencia, pacientes.tipodeficiencia, escolaridade.descescolaridade, escola.descescola, profissao.descprofissao,"
+          		+ " encaminhado.descencaminhado, formatransporte.descformatransporte "
+          		+ "from hosp.pacientes left join hosp.escolaridade on pacientes.id_escolaridade=escolaridade.id_escolaridade "
+          		+ " left join hosp.escola on pacientes.id_escola=escola.id_escola "
+          		+ "left join hosp.profissao on pacientes.id_profissao=profissao.id_profissao "
+          		+ "left join hosp.encaminhado on pacientes.id_encaminhado=encaminhado.id_encaminhado "
+          		+ "left join hosp.formatransporte on pacientes.id_formatransporte=formatransporte.id_formatransporte order by pacientes.nome";
 
           ArrayList<PacienteBean> lista = new ArrayList();
 
@@ -231,6 +305,12 @@ public class PacienteDAO {
 
               while (rs.next()) {
               	PacienteBean p = new PacienteBean();
+              	EscolaridadeBean es = new EscolaridadeBean();
+              	ProfissaoBean pf = new ProfissaoBean();
+              	EscolaBean e = new EscolaBean();
+              	EncaminhadoBean en = new EncaminhadoBean();
+              	FormaTransporteBean ft = new FormaTransporteBean();
+              	
 	                p.setId_paciente(rs.getLong("id_paciente"));
 	                p.setNome(rs.getString("nome").toLowerCase());
 	                p.setDtanascimento(rs.getDate("dtanascimento"));
@@ -269,9 +349,9 @@ public class PacienteDAO {
 	                p.setFolha(rs.getInt("folha"));
 	                p.setDataExpedicao2(rs.getDate("dtaregistro"));
 	                p.setAssociado(rs.getString("contribuinte"));
-	                p.getEscolaridade().setCodescolaridade(rs.getInt("codescolaridade"));
-	                p.getEscola().setCodEscola(rs.getInt("codescola"));
-	                p.getProfissao().setCodprofissao(rs.getInt("codprofissao"));
+	                p.getEscolaridade().setCodescolaridade(rs.getInt("id_escolaridade"));
+	                p.getEscola().setCodEscola(rs.getInt("id_escola"));
+	                p.getProfissao().setCodprofissao(rs.getInt("id_profissao"));
 	                p.setTrabalha(rs.getString("trabalha"));
 	                p.setLocaltrabalha(rs.getString("localtrabalha"));
 	                p.setCodparentesco(rs.getInt("codparentesco"));
@@ -279,11 +359,15 @@ public class PacienteDAO {
 	                p.setRgresp(rs.getString("rgresp"));
 	                p.setCpfresp(rs.getString("cpfresp"));
 	                p.setDataNascimentoresp(rs.getDate("dtanascimentoresp"));
-	                p.getEncaminhado().setCodencaminhado(rs.getInt("codencaminhado"));
-	                p.getFormatransporte().setCodformatransporte(rs.getInt("codformatransporte"));
+	                p.getEncaminhado().setCodencaminhado(rs.getInt("id_encaminhado"));
+	                p.getFormatransporte().setCodformatransporte(rs.getInt("id_formatransporte"));
 	                p.setDeficiencia(rs.getString("deficiencia"));
 	                p.setTipoDeficiencia(rs.getString("tipodeficiencia"));
-	                
+	                es.setDescescolaridade(rs.getString("descescolaridade"));
+	                e.setDescescola(rs.getString("descescola"));
+	                pf.setDescprofissao(rs.getString("descprofissao"));
+	                en.setDescencaminhado(rs.getString("descencaminhado"));
+	                ft.setDescformatransporte(rs.getString("descformatransporte"));
 	                 
 	                
 	                lista.add(p);
@@ -347,7 +431,7 @@ public class PacienteDAO {
 			while (rs.next()) {
 				EscolaBean p = new EscolaBean();
 				System.out.println("|1|");
-				p.setCodEscola(rs.getInt("codescola"));
+				p.setCodEscola(rs.getInt("id_escola"));
 				p.setDescescola(rs.getString("descescola").toLowerCase());
 				p.setCodtipoescola(rs.getInt("codtipoescola"));
 
@@ -372,7 +456,7 @@ public class PacienteDAO {
 
 		try {
 
-			String sql = "select codprofissao from hosp.paciente where id_paciente=? order by codprofissao";
+			String sql = "select id_profissao from hosp.paciente where id_paciente=? order by id_profissao";
 
 			ps = conexao.prepareStatement(sql);
 			ps.setInt(1, i);
