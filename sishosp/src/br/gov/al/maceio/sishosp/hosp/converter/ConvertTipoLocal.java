58f7724ef0679bbd47af338aee11eafbc319b6d6
Thulio Buarque <thulio.buarque2@gmail.com>
2016-08-30 23:13:57 -0300

    usuario

---
+++ sishosp/src/br/gov/al/maceio/sishosp/hosp/converter/ConvertTipoLocal.java
@@ -0,0 +1,40 @@
+package br.gov.al.maceio.sishosp.hosp.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(value = "conTpLocal")
+public class ConvertTipoLocal implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext arg0, UIComponent arg1, String valor) {
+		// TODO Auto-generated method stub
+		
+		return valor;
+	}
+
+	@Override
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object objeto) {
+		// TODO Auto-generated method stub
+
+		
+		String descTipo = objeto.toString();
+
+		
+		if (descTipo == null || descTipo.trim().length() == 0
+				|| descTipo == "") {
+
+			return null;
+
+		} else {
+
+			
+			return descTipo;
+		}
+
+	}
+
+
+}
