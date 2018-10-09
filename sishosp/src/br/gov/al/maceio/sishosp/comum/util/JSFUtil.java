240e58bf0a921df912e8a4575d4a93779a9542d7
Argemiro <argemiro.pcosta@gmail.com>
2018-10-09 20:49:22 -0300

    - Criar JSFUtil para gerar mensagens e manipular dialogs a partir do código java.

---
+++ sishosp/src/br/gov/al/maceio/sishosp/comum/util/JSFUtil.java
@@ -0,0 +1,40 @@
+package br.gov.al.maceio.sishosp.comum.util;
+
+import org.primefaces.context.RequestContext;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public final class JSFUtil {
+	
+	public static void adicionarMensagemSucesso(String mensagem, String informativo){
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, mensagem, informativo);
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, msg);
+	}
+	
+	public static void adicionarMensagemErro(String mensagem, String informativo){
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagem, informativo);
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, msg);
+	}
+	
+	public static void adicionarMensagemAdvertencia(String mensagem, String informativo){
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN, mensagem, informativo);
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, msg);
+	}
+
+	public static RequestContext getRequestContext() {
+		return RequestContext.getCurrentInstance();
+	}
+
+	public static void abrirDialog(String dialog) {
+		getRequestContext().execute("PF('" + dialog + "').show();");
+	}
+
+	public static void fecharDialog(String dialog) {
+		getRequestContext().execute("PF('" + dialog + "').hide();");
+	}
+
+}
