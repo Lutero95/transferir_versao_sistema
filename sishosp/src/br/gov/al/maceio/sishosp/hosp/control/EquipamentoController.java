b17deb94042b0f552afb54c19b62a5329a7db2a7
Argemiro <argemiro.pcosta@gmail.com>
2018-10-11 22:24:40 -0300

    Cadastro de equipamento finalizado.

--- sishosp/src/br/gov/al/maceio/sishosp/hosp/control/EquipamentoController.java
+++ sishosp/src/br/gov/al/maceio/sishosp/hosp/control/EquipamentoController.java
@@ -6,201 +6,165 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
-import org.primefaces.context.RequestContext;
+import br.gov.al.maceio.sishosp.comum.util.JSFUtil;
+import br.gov.al.maceio.sishosp.comum.util.RedirecionarUtil;
 
 import br.gov.al.maceio.sishosp.comum.exception.ProjetoException;
-import br.gov.al.maceio.sishosp.hosp.dao.EncaminhadoDAO;
 import br.gov.al.maceio.sishosp.hosp.dao.EquipamentoDAO;
 import br.gov.al.maceio.sishosp.hosp.model.EquipamentoBean;
-import br.gov.al.maceio.sishosp.hosp.model.EquipeBean;
 
 @ManagedBean(name = "EquipamentoController")
 @ViewScoped
 public class EquipamentoController implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private EquipamentoBean equipamento;
-	private List<EquipamentoBean> listaEquipamentos;
-	private Integer tipoBuscar;
-	private String descricaoBusca;
-	private int tipo;
-	private String cabecalho;
+    private static final long serialVersionUID = 1L;
+    private EquipamentoBean equipamento;
+    private List<EquipamentoBean> listaEquipamentos;
+    private Integer tipoBuscar;
+    private int tipo;
+    private String cabecalho;
+    private EquipamentoDAO eDao = new EquipamentoDAO();
 
-	private Integer abaAtiva = 0;
+    //CONSTANTES
+    private static final String ENDERECO_CADASTRO = "cadastroEquipamento?faces-redirect=true";
+    private static final String ENDERECO_TIPO = "&amp;tipo=";
+    private static final String ENDERECO_ID = "&amp;id=";
+    private static final String CABECALHO_INCLUSAO = "Inclusão de Equipamento";
+    private static final String CABECALHO_ALTERACAO = "Alteração de Equipamento";
 
-	public EquipamentoController() {
-		equipamento = new EquipamentoBean();
+    public EquipamentoController() {
+        equipamento = new EquipamentoBean();
+        listaEquipamentos = new ArrayList<>();
+        listaEquipamentos = null;
+        this.cabecalho = "";
 
-		listaEquipamentos = new ArrayList<>();
-		listaEquipamentos = null;
-		this.descricaoBusca = new String();
-		this.cabecalho = "";
+    }
 
-	}
+    public String redirectEdit() {
+        return RedirecionarUtil.redirectEdit(ENDERECO_CADASTRO, ENDERECO_ID, this.equipamento.getId_equipamento(), ENDERECO_TIPO, tipo);
+    }
 
-	public String redirectEdit() {
-		return "cadastroEquipamento?faces-redirect=true&amp;id="
-				+ this.equipamento.getId_equipamento() + "&amp;tipo=" + tipo;
-	}
+    public String redirectInsert() {
+        return RedirecionarUtil.redirectInsert(ENDERECO_CADASTRO, ENDERECO_TIPO, tipo);
+    }
 
-	public String redirectInsert() {
-		return "cadastroEquipamento?faces-redirect=true&amp;tipo=" + tipo;
-	}
 
-	public void limparDados() {
-		equipamento = new EquipamentoBean();
-		listaEquipamentos = new ArrayList<>();
-		this.descricaoBusca = new String();
+    public void limparDados() {
+        equipamento = new EquipamentoBean();
+        listaEquipamentos = new ArrayList<>();
+    }
 
-	}
+    public void gravarEquipamento() throws ProjetoException, SQLException {
+        boolean cadastrou = eDao.gravarEquipamento(equipamento);
 
-	public void gravarEquipamento() throws ProjetoException, SQLException {
-		EquipamentoDAO gDao = new EquipamentoDAO();
-		boolean cadastrou = gDao.gravarEquipamento(equipamento);
+        if (cadastrou == true) {
+            limparDados();
+            JSFUtil.adicionarMensagemSucesso("Equipamento cadastrado com sucesso!", "Sucesso");
+        } else {
+            JSFUtil.adicionarMensagemErro("Ocorreu um erro durante o cadastro!", "Erro");
+        }
+    }
 
-		if (cadastrou == true) {
-			limparDados();
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-					"Equipamento cadastrado com sucesso!", "Sucesso");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-		} else {
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					"Ocorreu um erro durante o cadastro!", "Erro");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-		}
-	}
+    public void alterarEquipamento() throws ProjetoException, SQLException {
+        boolean alterou = eDao.alterarEquipamento(equipamento);
 
-	public void alterarEquipamento() throws ProjetoException, SQLException {
-		EquipamentoDAO gDao = new EquipamentoDAO();
-		boolean alterou = gDao.alterarEquipamento(equipamento);
-		if (alterou == true) {
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-					"Equipamento alterado com sucesso!", "Sucesso");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-		} else {
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					"Ocorreu um erro durante o cadastro!", "Erro");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-		}
-		listaEquipamentos = gDao.listarEquipamentos();
+        if (alterou == true) {
+            JSFUtil.adicionarMensagemSucesso("Equipamento alterado com sucesso!", "Sucesso");
+        } else {
+            JSFUtil.adicionarMensagemErro("Ocorreu um erro durante a alteração!", "Erro");
+        }
+        listaEquipamentos = eDao.listarEquipamentos();
 
-	}
+    }
 
-	public void excluirEquipamento() throws ProjetoException, SQLException {
-		EquipamentoDAO gDao = new EquipamentoDAO();
-		boolean ok = gDao.excluirEquipamento(equipamento);
-		if (ok == true) {
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-					"Equipamento excluído com sucesso!", "Sucesso");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-			RequestContext.getCurrentInstance().execute(
-					"PF('dialogAtencao').hide();");
-		} else {
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					"Ocorreu um erro durante a exclusao!", "Erro");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
+    public void excluirEquipamento() throws ProjetoException, SQLException {
+        boolean excluiu = eDao.excluirEquipamento(equipamento);
 
-			RequestContext.getCurrentInstance().execute(
-					"PF('dialogAtencao').hide();");
-		}
-		listaEquipamentos = gDao.listarEquipamentos();
-	}
+        if (excluiu == true) {
+            JSFUtil.adicionarMensagemSucesso("Equipamento excluído com sucesso!", "Sucesso");
+            JSFUtil.fecharDialog("dialogExclusao");
+        } else {
+            JSFUtil.adicionarMensagemErro("Ocorreu um erro durante a exclusão!", "Erro");
+            JSFUtil.fecharDialog("dialogExclusao");
+        }
+        listaEquipamentos = eDao.listarEquipamentos();
+    }
 
-	public void getEditEquipamento() throws ProjetoException {
-		FacesContext facesContext = FacesContext.getCurrentInstance();
-		Map<String, String> params = facesContext.getExternalContext()
-				.getRequestParameterMap();
-		if (params.get("id") != null) {
-			Integer id = Integer.parseInt(params.get("id"));
-			tipo = Integer.parseInt(params.get("tipo"));
-			EquipamentoDAO cDao = new EquipamentoDAO();
-			this.equipamento = cDao.buscaEquipamentoPorId(id);
-		} else {
-			tipo = Integer.parseInt(params.get("tipo"));
+    public void getEditEquipamento() throws ProjetoException {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        Map<String, String> params = facesContext.getExternalContext()
+                .getRequestParameterMap();
+        if (params.get("id") != null) {
+            Integer id = Integer.parseInt(params.get("id"));
+            tipo = Integer.parseInt(params.get("tipo"));
+            this.equipamento = eDao.buscaEquipamentoPorId(id);
+        } else {
+            tipo = Integer.parseInt(params.get("tipo"));
 
-		}
+        }
 
-	}
+    }
 
-	public List<EquipamentoBean> listaEquipamentoAutoComplete(String query)
-			throws ProjetoException {
-		EquipamentoDAO cDao = new EquipamentoDAO();
-		List<EquipamentoBean> result = cDao
-				.listarEquipamentoAutoComplete(query);
+    public List<EquipamentoBean> listarEquipamentos()
+            throws ProjetoException, SQLException {
+        listaEquipamentos = eDao.listarEquipamentos();
 
-		return result;
-	}
+        return listaEquipamentos;
+    }
 
-	public EquipamentoBean getEquipamento() {
-		return equipamento;
-	}
+    public List<EquipamentoBean> listaEquipamentoAutoComplete(String query)
+            throws ProjetoException {
+        List<EquipamentoBean> result = eDao.listarEquipamentoAutoComplete(query);
 
-	public void setEquipamento(EquipamentoBean equipamento) {
-		this.equipamento = equipamento;
-	}
+        return result;
+    }
 
-	public Integer getTipoBuscar() {
-		return tipoBuscar;
-	}
+    public EquipamentoBean getEquipamento() {
+        return equipamento;
+    }
 
-	public void setTipoBuscar(Integer tipoBuscar) {
-		this.tipoBuscar = tipoBuscar;
-	}
+    public void setEquipamento(EquipamentoBean equipamento) {
+        this.equipamento = equipamento;
+    }
 
-	public String getDescricaoBusca() {
-		return descricaoBusca;
-	}
+    public Integer getTipoBuscar() {
+        return tipoBuscar;
+    }
 
-	public void setDescricaoBusca(String descricaoBusca) {
-		this.descricaoBusca = descricaoBusca;
-	}
+    public void setTipoBuscar(Integer tipoBuscar) {
+        this.tipoBuscar = tipoBuscar;
+    }
 
-	public Integer getAbaAtiva() {
-		return abaAtiva;
-	}
+    public int getTipo() {
+        return tipo;
+    }
 
-	public void setAbaAtiva(Integer abaAtiva) {
-		this.abaAtiva = abaAtiva;
-	}
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
 
-	public int getTipo() {
-		return tipo;
-	}
+    public void setCabecalho(String cabecalho) {
+        this.cabecalho = cabecalho;
+    }
 
-	public void setTipo(int tipo) {
-		this.tipo = tipo;
-	}
+    public String getCabecalho() {
+        if (this.tipo == 1) {
+            cabecalho = CABECALHO_INCLUSAO;
+        } else if (this.tipo == 2) {
+            cabecalho = CABECALHO_ALTERACAO;
+        }
+        return cabecalho;
+    }
 
-	public void setCabecalho(String cabecalho) {
-		this.cabecalho = cabecalho;
-	}
+    public List<EquipamentoBean> getListaEquipamentos() {
+        return listaEquipamentos;
+    }
 
-	public String getCabecalho() {
-		if (this.tipo == 1) {
-			cabecalho = "Inclusão de Equipamento";
-		} else if (this.tipo == 2) {
-			cabecalho = "Alteração de Equipamento";
-		}
-		return cabecalho;
-	}
-
-	public List<EquipamentoBean> getListaEquipamentos() throws SQLException,
-			ProjetoException {
-		if (listaEquipamentos == null) {
-
-			EquipamentoDAO fdao = new EquipamentoDAO();
-			listaEquipamentos = fdao.listarEquipamentos();
-		}
-		return listaEquipamentos;
-	}
-
-	public void setListaEquipamentos(List<EquipamentoBean> listaEquipamentos) {
-		this.listaEquipamentos = listaEquipamentos;
-	}
-
+    public void setListaEquipamentos(List<EquipamentoBean> listaEquipamentos) {
+        this.listaEquipamentos = listaEquipamentos;
+    }
 }
--- sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/EquipamentoDAO.java
+++ sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/EquipamentoDAO.java
@@ -10,205 +10,173 @@ import java.util.List;
 import br.gov.al.maceio.sishosp.comum.exception.ProjetoException;
 import br.gov.al.maceio.sishosp.comum.util.ConnectionFactory;
 import br.gov.al.maceio.sishosp.hosp.model.EquipamentoBean;
-import br.gov.al.maceio.sishosp.hosp.model.EquipeBean;
 
 public class EquipamentoDAO {
-	private Connection conexao = null;
-	Connection con = null;
-	PreparedStatement ps = null;
+    Connection con = null;
+    PreparedStatement ps = null;
 
-	public boolean gravarEquipamento(EquipamentoBean equip)
-			throws SQLException, ProjetoException {
+    public boolean gravarEquipamento(EquipamentoBean equip) {
+        Boolean retorno = false;
+        String sql = "insert into hosp.tipoaparelho (desctipoaparelho) values (?);";
 
-		String sql = "insert into hosp.tipoaparelho (desctipoaparelho) values (?);";
-		try {
-			con = ConnectionFactory.getConnection();
-			ps = con.prepareStatement(sql);
-			ps.setString(1, equip.getDescEquipamento().toUpperCase());
-			ps.execute();
-			con.commit();
-			return true;
-		} catch (SQLException ex) {
-			throw new RuntimeException(ex);
-		} finally {
-			try {
-				con.close();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				System.exit(1);
-			}
-		}
-	}
+        try {
+            con = ConnectionFactory.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, equip.getDescEquipamento().toUpperCase());
+            ps.execute();
 
-	public ArrayList<EquipamentoBean> listarEquipamentos() throws SQLException,
-			ProjetoException {
-		String sql = "select id, desctipoaparelho from hosp.tipoaparelho order by desctipoaparelho";
+            con.commit();
+            retorno = true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                con.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return retorno;
+        }
+    }
 
-		ArrayList<EquipamentoBean> lista = new ArrayList();
+    public ArrayList<EquipamentoBean> listarEquipamentos() throws ProjetoException {
+        ArrayList<EquipamentoBean> lista = new ArrayList();
+        String sql = "select id, desctipoaparelho from hosp.tipoaparelho order by desctipoaparelho";
 
-		try {
-			conexao = ConnectionFactory.getConnection();
-			PreparedStatement stm = conexao.prepareStatement(sql);
-			ResultSet rs = stm.executeQuery();
+        try {
+            con = ConnectionFactory.getConnection();
+            PreparedStatement stm = con.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
 
-			while (rs.next()) {
-				EquipamentoBean e = new EquipamentoBean();
-				e.setId_equipamento(rs.getInt("id"));
-				e.setDescEquipamento(rs.getString("desctipoaparelho"));
+            while (rs.next()) {
+                EquipamentoBean e = new EquipamentoBean();
+                e.setId_equipamento(rs.getInt("id"));
+                e.setDescEquipamento(rs.getString("desctipoaparelho"));
 
-				lista.add(e);
-			}
-		} catch (SQLException ex) {
-			throw new RuntimeException(ex);
-		} finally {
-			try {
-				conexao.close();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				System.exit(1);
-			}
-		}
-		return lista;
-	}
+                lista.add(e);
+            }
 
-	public List<EquipamentoBean> listarEquipamentoBusca(String descricao,
-			Integer tipo) throws ProjetoException {
-		List<EquipamentoBean> lista = new ArrayList<>();
-		String sql = "select id, desctipoaparelho from hosp.tipoaparelho ";
-		if (tipo == 1) {
-			sql += " where desctipoaparelho LIKE ?  order by desctipoaparelho";
-		}
-		try {
-			con = ConnectionFactory.getConnection();
-			PreparedStatement stm = con.prepareStatement(sql);
-			stm.setString(1, "%" + descricao.toUpperCase() + "%");
-			ResultSet rs = stm.executeQuery();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                con.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return lista;
+        }
+    }
 
-			while (rs.next()) {
-				EquipamentoBean equip = new EquipamentoBean();
-				equip.setId_equipamento(rs.getInt("id"));
-				equip.setDescEquipamento(rs.getString("desctipoaparelho"));
+    public Boolean alterarEquipamento(EquipamentoBean equip) {
+        Boolean retorno = false;
+        String sql = "update hosp.tipoaparelho set desctipoaparelho = ? where id = ?";
 
-				lista.add(equip);
-			}
-		} catch (SQLException ex) {
-			throw new RuntimeException(ex);
-		} finally {
-			try {
-				con.close();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				System.exit(1);
-			}
-		}
+        try {
+            con = ConnectionFactory.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, equip.getDescEquipamento().toUpperCase());
+            stmt.setInt(2, equip.getId_equipamento());
+            stmt.executeUpdate();
 
-		return lista;
-	}
+            con.commit();
+            retorno = true;
 
-	public Boolean alterarEquipamento(EquipamentoBean equip)
-			throws ProjetoException {
-		String sql = "update hosp.tipoaparelho set desctipoaparelho = ? where id = ?";
-		try {
-			con = ConnectionFactory.getConnection();
-			PreparedStatement stmt = con.prepareStatement(sql);
-			stmt.setString(1, equip.getDescEquipamento().toUpperCase());
-			stmt.setInt(2, equip.getId_equipamento());
-			stmt.executeUpdate();
-			con.commit();
-			return true;
-		} catch (SQLException ex) {
-			throw new RuntimeException(ex);
-		} finally {
-			try {
-				con.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
-	}
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                con.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return retorno;
+        }
+    }
 
-	public Boolean excluirEquipamento(EquipamentoBean equip)
-			throws ProjetoException {
-		String sql = "delete from hosp.tipoaparelho where id = ?";
-		try {
-			con = ConnectionFactory.getConnection();
-			PreparedStatement stmt = con.prepareStatement(sql);
-			stmt.setLong(1, equip.getId_equipamento());
-			stmt.execute();
-			con.commit();
-			return true;
-		} catch (SQLException ex) {
-			throw new RuntimeException(ex);
-		} finally {
-			try {
-				con.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
-	}
+    public Boolean excluirEquipamento(EquipamentoBean equip) {
+        Boolean retorno = false;
+        String sql = "delete from hosp.tipoaparelho where id = ?";
 
-	public EquipamentoBean buscaEquipamentoPorId(Integer i)
-			throws ProjetoException {
-		String sql = "select id, desctipoaparelho from hosp.tipoaparelho where id =? order by desctipoaparelho";
-		try {
-			con = ConnectionFactory.getConnection();
-			ps = con.prepareStatement(sql);
-			ps.setInt(1, i);
-			ResultSet rs = ps.executeQuery();
-			EquipamentoBean g = new EquipamentoBean();
-			while (rs.next()) {
-				g.setId_equipamento(rs.getInt("id"));
-				g.setDescEquipamento(rs.getString("desctipoaparelho"));
+        try {
+            con = ConnectionFactory.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setLong(1, equip.getId_equipamento());
+            stmt.execute();
+            con.commit();
+            retorno = true;
 
-			}
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                con.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return retorno;
+        }
+    }
 
-			return g;
-		} catch (Exception sqle) {
-			sqle.printStackTrace();
-			throw new ProjetoException(sqle);
+    public EquipamentoBean buscaEquipamentoPorId(Integer i) {
 
-		} finally {
-			try {
-				con.close();
-			} catch (Exception sqlc) {
-				sqlc.printStackTrace();
-				System.exit(1);
-				// TODO: handle exception
-			}
+        String sql = "select id, desctipoaparelho from hosp.tipoaparelho where id =? order by desctipoaparelho";
+        try {
+            con = ConnectionFactory.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, i);
+            ResultSet rs = ps.executeQuery();
+            EquipamentoBean equip = new EquipamentoBean();
+            while (rs.next()) {
+                equip.setId_equipamento(rs.getInt("id"));
+                equip.setDescEquipamento(rs.getString("desctipoaparelho"));
 
-		}
-	}
-	
-	public List<EquipamentoBean> listarEquipamentoAutoComplete(String descricao)
-			throws ProjetoException {
-		List<EquipamentoBean> lista = new ArrayList<>();
-		String sql = "select id, desctipoaparelho from hosp.tipoaparelho where desctipoaparelho like ? order by desctipoaparelho";
+            }
+            return equip;
+        } catch (SQLException | ProjetoException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                con.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 
-		try {
-			con = ConnectionFactory.getConnection();
-			PreparedStatement stm = con.prepareStatement(sql);
-			stm.setString(1, "%" + descricao.toUpperCase() + "%");
-			ResultSet rs = stm.executeQuery();
+    public List<EquipamentoBean> listarEquipamentoAutoComplete(String descricao) {
+        List<EquipamentoBean> lista = new ArrayList<>();
+        String sql = "select id, desctipoaparelho from hosp.tipoaparelho where desctipoaparelho like ? order by desctipoaparelho";
 
-			while (rs.next()) {
-				EquipamentoBean equipamento = new EquipamentoBean();
-				equipamento.setId_equipamento(rs.getInt("id"));
-				equipamento.setDescEquipamento(rs.getString("desctipoaparelho"));
+        try {
+            con = ConnectionFactory.getConnection();
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, "%" + descricao.toUpperCase() + "%");
+            ResultSet rs = stm.executeQuery();
 
-				lista.add(equipamento);
-			}
-		} catch (SQLException ex) {
-			throw new RuntimeException(ex);
-		} finally {
-			try {
-				con.close();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				System.exit(1);
-			}
-		}
-		return lista;
-	}
+            while (rs.next()) {
+                EquipamentoBean equipamento = new EquipamentoBean();
+                equipamento.setId_equipamento(rs.getInt("id"));
+                equipamento.setDescEquipamento(rs.getString("desctipoaparelho"));
+
+                lista.add(equipamento);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                con.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return lista;
+        }
+    }
 
 }
