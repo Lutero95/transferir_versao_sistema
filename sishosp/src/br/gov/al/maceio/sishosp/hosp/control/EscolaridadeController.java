5e14adb7fe51a7032a3b973bc9cfaf73136eba20
Argemiro <argemiro.pcosta@gmail.com>
2018-10-15 22:38:00 -0300

    - Cadastro de escolaridade.

--- sishosp/src/br/gov/al/maceio/sishosp/hosp/control/EscolaridadeController.java
+++ sishosp/src/br/gov/al/maceio/sishosp/hosp/control/EscolaridadeController.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
-import org.primefaces.context.RequestContext;
+import br.gov.al.maceio.sishosp.comum.util.JSFUtil;
 
 import br.gov.al.maceio.sishosp.comum.exception.ProjetoException;
+import br.gov.al.maceio.sishosp.comum.util.RedirecionarUtil;
 import br.gov.al.maceio.sishosp.hosp.dao.EscolaridadeDAO;
 import br.gov.al.maceio.sishosp.hosp.model.EscolaridadeBean;
 
@@ -20,241 +20,131 @@ import br.gov.al.maceio.sishosp.hosp.model.EscolaridadeBean;
 @ViewScoped
 public class EscolaridadeController implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private Integer abaAtiva = 0;
-	private EscolaridadeBean escolaridade;
+    private static final long serialVersionUID = 1L;
+    private EscolaridadeBean escolaridade;
+    private String cabecalho;
+    private int tipo;
+    private List<EscolaridadeBean> listaEscolaridade;
+    private EscolaridadeDAO eDao = new EscolaridadeDAO();
 
-	private String cabecalho;
+    //CONSTANTES
+    private static final String ENDERECO_CADASTRO = "cadastroEscolaridade?faces-redirect=true";
+    private static final String ENDERECO_TIPO = "&amp;tipo=";
+    private static final String ENDERECO_ID = "&amp;id=";
+    private static final String CABECALHO_INCLUSAO = "Inclusão de Escolaridade";
+    private static final String CABECALHO_ALTERACAO = "Alteração de Escolaridade";
 
-	// BUSCAS
-	private int tipo;
-	private Integer tipoBuscaEscolaridade;
-	private String campoBuscaEscolaridade;
-	private String statusEscolaridade;
-	private List<EscolaridadeBean> listaEscolaridade;
+    public EscolaridadeController() {
+        escolaridade = new EscolaridadeBean();
+        listaEscolaridade = new ArrayList<>();
+    }
 
-	public EscolaridadeController() {
+    public String redirectEdit() {
+        return RedirecionarUtil.redirectEdit(ENDERECO_CADASTRO, ENDERECO_ID, this.escolaridade.getCodescolaridade(), ENDERECO_TIPO, tipo);
+    }
 
-		escolaridade = new EscolaridadeBean();
+    public String redirectInsert() {
+        return RedirecionarUtil.redirectInsert(ENDERECO_CADASTRO, ENDERECO_TIPO, tipo);
+    }
 
-		// BUSCA
-		tipoBuscaEscolaridade = 1;
-		campoBuscaEscolaridade = "";
-		statusEscolaridade = "P";
+    public void getEditEscolaridade() throws ProjetoException {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        Map<String, String> params = facesContext.getExternalContext()
+                .getRequestParameterMap();
+        if (params.get("id") != null) {
+            Integer id = Integer.parseInt(params.get("id"));
+            tipo = Integer.parseInt(params.get("tipo"));
 
-		listaEscolaridade = new ArrayList<>();
+            this.escolaridade = eDao.buscaescolaridadecodigo(id);
+        } else {
 
-	}
+            tipo = Integer.parseInt(params.get("tipo"));
 
-	public void gravarEscolaridade() throws ProjetoException {
-		EscolaridadeDAO udao = new EscolaridadeDAO();
-		boolean cadastrou = udao.cadastrar(escolaridade);
+        }
 
-		if (cadastrou == true) {
-			limparDados();
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-					"Escolaridade cadastrada com sucesso!", "Sucesso");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
 
-			// return
-			// "gerenciarEscolaridade?faces-redirect=true&amp;tipo="+tipo+"&amp;sucesso=Escolaridade cadastrada com sucesso!";
+    public void limparDados() {
+        escolaridade = new EscolaridadeBean();
+    }
 
-		} else {
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					"Ocorreu um erro durante o cadastro!", "Erro");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-			// return "";
-		}
+    public void gravarEscolaridade() {
 
-	}
+        boolean cadastrou = eDao.cadastrar(escolaridade);
 
-	public void alterarEscolaridade() throws ProjetoException {
+        if (cadastrou == true) {
+            limparDados();
+            JSFUtil.adicionarMensagemSucesso("Escolaridade cadastrada com sucesso!", "Sucesso");
+        } else {
+            JSFUtil.adicionarMensagemErro("Ocorreu um erro durante o cadastro!", "Erro");
+        }
 
-		EscolaridadeDAO rdao = new EscolaridadeDAO();
-		boolean alterou = rdao.alterar(escolaridade);
+    }
 
-		if (alterou == true) {
-			// limparDados();
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-					"Escolaridade alterada com sucesso!", "Sucesso");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-			// return
-			// "/pages/sishosp/gerenciarEscolaridade.faces?faces-redirect=true";
-			// RequestContext.getCurrentInstance().execute("dlgAltMenu.hide();");
-		} else {
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					"Ocorreu um erro durante o cadastro!", "Erro");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-			// return "";
-			// RequestContext.getCurrentInstance().execute("dlgAltMenu.hide();");
-		}
+    public void alterarEscolaridade() {
 
-	}
+        boolean alterou = eDao.alterar(escolaridade);
 
-	public void excluirEscolaridade() throws ProjetoException {
-		EscolaridadeDAO udao = new EscolaridadeDAO();
+        if (alterou == true) {
+            JSFUtil.adicionarMensagemSucesso("Escolaridade alterada com sucesso!", "Sucesso");
+        } else {
+            JSFUtil.adicionarMensagemErro("Ocorreu um erro durante a alteração!", "Erro");
+        }
 
-		boolean excluio = udao.excluir(escolaridade);
+    }
 
-		if (excluio == true) {
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-					"Escolaridade excluída com sucesso!", "Sucesso");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-			listaEscolaridade = null;
-			RequestContext.getCurrentInstance().execute(
-					"PF('dialogAtencao').hide();");
-			listarEscolaridade();
-		} else {
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					"Ocorreu um erro durante a exclusao!", "Erro");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
+    public void excluirEscolaridade() throws ProjetoException {
 
-			RequestContext.getCurrentInstance().execute(
-					"PF('dialogAtencao').hide();");
-		}
-	}
+        boolean excluiu = eDao.excluir(escolaridade);
 
-	public void buscarEscolaridades() throws ProjetoException {
+        if (excluiu == true) {
+            JSFUtil.adicionarMensagemSucesso("Escolaridade excluída com sucesso!", "Sucesso");
+            JSFUtil.fecharDialog("dialogExclusao");
+            listaEscolaridade = null;
+            buscarEscolaridades();
+        } else {
+            JSFUtil.adicionarMensagemErro("Ocorreu um erro durante a exclusão!", "Erro");
+            JSFUtil.fecharDialog("dialogExclusao");
+        }
+    }
 
-		List<EscolaridadeBean> listaAux = null;
-		listaEscolaridade = new ArrayList<>();
+    public List<EscolaridadeBean> listarEscolaridades() throws ProjetoException {
+        listaEscolaridade = eDao.listaEscolaridade();
 
-		EscolaridadeDAO adao = new EscolaridadeDAO();
+        return listaEscolaridade;
+    }
 
-		listaAux = adao.buscarTipoEscolaridade(campoBuscaEscolaridade,
-				tipoBuscaEscolaridade);
+    public void buscarEscolaridades() throws ProjetoException {
+        listaEscolaridade = eDao.listaEscolaridade();
+    }
 
-		if (listaAux != null && listaAux.size() > 0) {
-			// listaAss = null;
-			listaEscolaridade = listaAux;
-		} else {
-			// listaAss = null;
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN,
-					"Nenhuma Escolaridade encontrada.", "Aviso");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-		}
+    public EscolaridadeBean getEscolaridade() {
+        return escolaridade;
+    }
 
-	}
+    public void setEscolaridade(EscolaridadeBean escolaridade) {
+        this.escolaridade = escolaridade;
+    }
 
-	public void limparBuscaDados() {
-		tipoBuscaEscolaridade = 1;
-		campoBuscaEscolaridade = "";
-		statusEscolaridade = "P";
-		listaEscolaridade = null;
-	}
+    public String getCabecalho() {
+        if (this.tipo == 1) {
+            cabecalho = CABECALHO_INCLUSAO;
+        } else if (this.tipo == 2) {
+            cabecalho = CABECALHO_ALTERACAO;
+        }
+        return cabecalho;
+    }
 
-	public String redirectEdit() {
-		return "cadastroEscolaridade?faces-redirect=true&amp;id="
-				+ this.escolaridade.getCodescolaridade() + "&amp;tipo=" + tipo;
-	}
+    public void setCabecalho(String cabecalho) {
+        this.cabecalho = cabecalho;
+    }
 
-	public String redirectInsert() {
-		FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
-				.put("tipo", tipo);
-		int tipoesc2 = (int) FacesContext.getCurrentInstance()
-				.getExternalContext().getSessionMap().get("tipo");
+    public int getTipo() {
+        return tipo;
+    }
 
-		return "cadastroEscolaridade?faces-redirect=true&amp;tipo=" + tipo;
-	}
-
-	public void getEditEscolaridade() throws ProjetoException {
-		FacesContext facesContext = FacesContext.getCurrentInstance();
-		Map<String, String> params = facesContext.getExternalContext()
-				.getRequestParameterMap();
-		if (params.get("id") != null) {
-			Integer id = Integer.parseInt(params.get("id"));
-			tipo = Integer.parseInt(params.get("tipo"));
-
-			EscolaridadeDAO udao = new EscolaridadeDAO();
-			this.escolaridade = udao.buscaescolaridadecodigo(id);
-		} else {
-
-			tipo = Integer.parseInt(params.get("tipo"));
-
-		}
-
-	}
-
-	public void limparDados() {
-		escolaridade = new EscolaridadeBean();
-
-	}
-
-	public EscolaridadeBean getEscolaridade() {
-		return escolaridade;
-	}
-
-	public void setEscolaridade(EscolaridadeBean escolaridade) {
-		this.escolaridade = escolaridade;
-	}
-
-	public Integer getAbaAtiva() {
-		return abaAtiva;
-	}
-
-	public void setAbaAtiva(Integer abaAtiva) {
-		this.abaAtiva = abaAtiva;
-	}
-
-	public Integer getTipoBuscaEscolaridade() {
-		return tipoBuscaEscolaridade;
-	}
-
-	public void setTipoBuscaEscolaridade(Integer tipoBuscaEscolaridade) {
-		this.tipoBuscaEscolaridade = tipoBuscaEscolaridade;
-	}
-
-	public String getCampoBuscaEscolaridade() {
-		return campoBuscaEscolaridade;
-	}
-
-	public void setCampoBuscaEscolaridade(String campoBuscaEscolaridade) {
-		this.campoBuscaEscolaridade = campoBuscaEscolaridade;
-	}
-
-	public String getStatusEscolaridade() {
-		return statusEscolaridade;
-	}
-
-	public void setStatusEscolaridade(String statusEscolaridade) {
-		this.statusEscolaridade = statusEscolaridade;
-	}
-
-	public void listarEscolaridade() throws ProjetoException {
-		EscolaridadeDAO fdao = new EscolaridadeDAO();
-		listaEscolaridade = fdao.listaEscolaridade();
-	}
-
-	public void setListaEscolaridade(List<EscolaridadeBean> listaEscolaridade) {
-		this.listaEscolaridade = listaEscolaridade;
-	}
-
-	public String getCabecalho() {
-		if (this.tipo == 1) {
-			cabecalho = "Inclusão de Escolaridade";
-		} else if (this.tipo == 2) {
-			cabecalho = "Alteração de Escolaridade";
-		}
-		return cabecalho;
-	}
-
-	public void setCabecalho(String cabecalho) {
-		this.cabecalho = cabecalho;
-	}
-
-	public int getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(int tipo) {
-		this.tipo = tipo;
-	}
-
-	/**
-	 * @return the listaEscolaridade
-	 */
-	public List<EscolaridadeBean> getListaEscolaridade() {
-		return listaEscolaridade;
-	}
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
 
 }
--- sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/EscolaridadeDAO.java
+++ sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/EscolaridadeDAO.java
@@ -12,254 +12,192 @@ import br.gov.al.maceio.sishosp.comum.util.ConnectionFactory;
 import br.gov.al.maceio.sishosp.hosp.model.EscolaridadeBean;
 
 public class EscolaridadeDAO {
-	private Connection conexao = null;
+    private Connection conexao = null;
 
-	public Boolean cadastrar(EscolaridadeBean escolaridade)
-			throws ProjetoException {
-		boolean cadastrou = false;
+    public Boolean cadastrar(EscolaridadeBean escolaridade) {
+        boolean retorno = false;
 
-		/*
-		 * PacienteBean user_session = (PacienteBean) FacesContext
-		 * .getCurrentInstance().getExternalContext().getSessionMap()
-		 * .get("obj_paciente");
-		 */
+        String sql = "insert into hosp.escolaridade (descescolaridade)"
+                + " values (?)";
 
-		String sql = "insert into hosp.escolaridade (descescolaridade)"
-				+ " values (?)";
-		// returning id_paciente
-		try {
-			conexao = ConnectionFactory.getConnection();
-			PreparedStatement stmt = conexao.prepareStatement(sql);
-			stmt.setString(1, escolaridade.getDescescolaridade().toUpperCase()
-					.trim());
-			// stmt.setString(2, paciente.getCpf().replaceAll("[^0-9]", ""));
-			// stmt.setBoolean(3, true);
-			// stmt.setInt(3, paciente.getIdpessoa());
-			/*
-			 * ResultSet rs = stmt.executeQuery(); if(rs.next()) { PacienteBean
-			 * p = paciente; String idRetorno = null; idRetorno =
-			 * String.valueOf(rs.getLong("id_usuario"));
-			 * p.setId_paciente(Long.parseLong(idRetorno));
-			 * 
-			 * }
-			 */
-			stmt.execute();
-			conexao.commit();
-			cadastrou = true;
-			conexao.close();
+        try {
+            conexao = ConnectionFactory.getConnection();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, escolaridade.getDescescolaridade().toUpperCase()
+                    .trim());
 
-			return cadastrou;
-		} catch (SQLException ex) {
-			throw new RuntimeException(ex);
-		}
-	}
+            stmt.execute();
+            conexao.commit();
+            retorno = true;
 
-	public Boolean alterar(EscolaridadeBean escolaridade)
-			throws ProjetoException {
-		boolean alterou = false;
-		String sql = "update hosp.escolaridade set descescolaridade = ? where id_escolaridade = ?";
-		try {
-			conexao = ConnectionFactory.getConnection();
-			PreparedStatement stmt = conexao.prepareStatement(sql);
-			stmt.setString(1, escolaridade.getDescescolaridade().toUpperCase());
-			stmt.setInt(2, escolaridade.getCodescolaridade());
-			stmt.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                conexao.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return retorno;
+        }
+    }
 
-			conexao.commit();
+    public Boolean alterar(EscolaridadeBean escolaridade) {
+        boolean retorno = false;
+        String sql = "update hosp.escolaridade set descescolaridade = ? where id_escolaridade = ?";
+        try {
+            conexao = ConnectionFactory.getConnection();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, escolaridade.getDescescolaridade().toUpperCase());
+            stmt.setInt(2, escolaridade.getCodescolaridade());
+            stmt.executeUpdate();
 
-			alterou = true;
+            conexao.commit();
 
-			return alterou;
-		} catch (SQLException ex) {
-			throw new RuntimeException(ex);
-		} finally {
-			try {
-				conexao.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
-	}
+            retorno = true;
 
-	public Boolean excluir(EscolaridadeBean escolaridade)
-			throws ProjetoException {
-		boolean excluir = false;
-		String sql = "delete from hosp.escolaridade where id_escolaridade = ?";
-		try {
-			conexao = ConnectionFactory.getConnection();
-			PreparedStatement stmt = conexao.prepareStatement(sql);
-			stmt.setInt(1, escolaridade.getCodescolaridade());
-			stmt.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                conexao.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return retorno;
+        }
+    }
 
-			conexao.commit();
+    public Boolean excluir(EscolaridadeBean escolaridade) {
+        boolean retorno = false;
+        String sql = "delete from hosp.escolaridade where id_escolaridade = ?";
+        try {
+            conexao = ConnectionFactory.getConnection();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, escolaridade.getCodescolaridade());
+            stmt.executeUpdate();
 
-			excluir = true;
+            conexao.commit();
 
-			return excluir;
-		} catch (SQLException ex) {
-			throw new RuntimeException(ex);
-		} finally {
-			try {
-				conexao.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
-	}
+            retorno = true;
 
-	public ArrayList<EscolaridadeBean> listaEscolaridade()
-			throws ProjetoException {
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                conexao.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return retorno;
+        }
+    }
 
-		String sql = "select * from hosp.escolaridade order by descescolaridade";
+    public ArrayList<EscolaridadeBean> listaEscolaridade()
+            throws ProjetoException {
 
-		ArrayList<EscolaridadeBean> lista = new ArrayList();
+        String sql = "select * from hosp.escolaridade order by descescolaridade";
 
-		try {
-			conexao = ConnectionFactory.getConnection();
-			PreparedStatement stm = conexao.prepareStatement(sql);
-			ResultSet rs = stm.executeQuery();
+        ArrayList<EscolaridadeBean> lista = new ArrayList();
 
-			while (rs.next()) {
-				EscolaridadeBean e = new EscolaridadeBean();
+        try {
+            conexao = ConnectionFactory.getConnection();
+            PreparedStatement stm = conexao.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
 
-				e.setCodescolaridade(rs.getInt("id_escolaridade"));
-				e.setDescescolaridade(rs.getString("descescolaridade")
-						.toUpperCase());
+            while (rs.next()) {
+                EscolaridadeBean e = new EscolaridadeBean();
 
-				lista.add(e);
-			}
-		} catch (SQLException ex) {
-			throw new RuntimeException(ex);
-		} finally {
-			try {
-				conexao.close();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				System.exit(1);
-			}
-		}
-		return lista;
-	}
+                e.setCodescolaridade(rs.getInt("id_escolaridade"));
+                e.setDescescolaridade(rs.getString("descescolaridade")
+                        .toUpperCase());
 
-	public EscolaridadeBean buscaescolaridadecodigo(Integer i)
-			throws ProjetoException {
-		PreparedStatement ps = null;
-		conexao = ConnectionFactory.getConnection();
+                lista.add(e);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                conexao.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return lista;
+    }
 
-		try {
+    public EscolaridadeBean buscaescolaridadecodigo(Integer i)
+            throws ProjetoException {
+        PreparedStatement ps = null;
+        conexao = ConnectionFactory.getConnection();
 
-			String sql = "select id_escolaridade, descescolaridade from hosp.escolaridade where id_escolaridade=? order by descescolaridade";
+        try {
 
-			ps = conexao.prepareStatement(sql);
-			ps.setInt(1, i);
-			ResultSet rs = ps.executeQuery();
+            String sql = "select id_escolaridade, descescolaridade from hosp.escolaridade where id_escolaridade=? order by descescolaridade";
 
-			EscolaridadeBean escolaridade = new EscolaridadeBean();
-			while (rs.next()) {
+            ps = conexao.prepareStatement(sql);
+            ps.setInt(1, i);
+            ResultSet rs = ps.executeQuery();
 
-				escolaridade.setCodescolaridade(rs.getInt(1));
-				escolaridade.setDescescolaridade(rs.getString(2));
+            EscolaridadeBean escolaridade = new EscolaridadeBean();
+            while (rs.next()) {
 
-			}
-			return escolaridade;
-		} catch (Exception sqle) {
+                escolaridade.setCodescolaridade(rs.getInt(1));
+                escolaridade.setDescescolaridade(rs.getString(2));
 
-			throw new ProjetoException(sqle);
+            }
+            return escolaridade;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                conexao.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 
-		} finally {
-			try {
-				conexao.close();
-			} catch (Exception sqlc) {
-				sqlc.printStackTrace();
-				System.exit(1);
-				// TODO: handle exception
-			}
+    public List<EscolaridadeBean> buscaescolaridade(String s)
+            throws ProjetoException {
+        PreparedStatement ps = null;
+        conexao = ConnectionFactory.getConnection();
 
-		}
-	}
+        try {
+            List<EscolaridadeBean> listaescolaridades = new ArrayList<EscolaridadeBean>();
+            String sql = "select id_escolaridade,id_escolaridade ||'-'|| descescolaridade descescolaridade from hosp.escolaridade "
+                    + " where upper(id_escolaridade ||'-'|| descescolaridade) like ? order by descescolaridade";
 
-	public List<EscolaridadeBean> buscaescolaridade(String s)
-			throws ProjetoException {
-		PreparedStatement ps = null;
-		conexao = ConnectionFactory.getConnection();
+            ps = conexao.prepareStatement(sql);
+            ps.setString(1, "%" + s.toUpperCase() + "%");
+            ResultSet rs = ps.executeQuery();
 
-		try {
-			List<EscolaridadeBean> listaescolaridades = new ArrayList<EscolaridadeBean>();
-			String sql = "select id_escolaridade,id_escolaridade ||'-'|| descescolaridade descescolaridade from hosp.escolaridade "
-					+ " where upper(id_escolaridade ||'-'|| descescolaridade) like ? order by descescolaridade";
+            List<EscolaridadeBean> colecao = new ArrayList<EscolaridadeBean>();
 
-			ps = conexao.prepareStatement(sql);
-			ps.setString(1, "%" + s.toUpperCase() + "%");
-			ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
 
-			List<EscolaridadeBean> colecao = new ArrayList<EscolaridadeBean>();
+                EscolaridadeBean escolaridade = new EscolaridadeBean();
+                escolaridade.setCodescolaridade(rs.getInt("id_escolaridade"));
+                escolaridade.setDescescolaridade(rs
+                        .getString("descescolaridade"));
+                colecao.add(escolaridade);
 
-			while (rs.next()) {
-
-				EscolaridadeBean escolaridade = new EscolaridadeBean();
-				escolaridade.setCodescolaridade(rs.getInt("id_escolaridade"));
-				escolaridade.setDescescolaridade(rs
-						.getString("descescolaridade"));
-				colecao.add(escolaridade);
-
-			}
-			return colecao;
-		} catch (Exception sqle) {
-
-			throw new ProjetoException(sqle);
-
-		} finally {
-			try {
-				conexao.close();
-			} catch (Exception sqlc) {
-				sqlc.printStackTrace();
-				System.exit(1);
-				// TODO: handle exception
-			}
-
-		}
-	}
-
-	public List<EscolaridadeBean> buscarTipoEscolaridade(String valor,
-			Integer tipo) throws ProjetoException {
-
-		String sql = "select escolaridade.id_escolaridade, escolaridade.descescolaridade from hosp.escolaridade where";
-
-		if (tipo == 1) {
-			sql += " escolaridade.descescolaridade like ? order by escolaridade.descescolaridade ";
-		}
-		List<EscolaridadeBean> lista = new ArrayList<>();
-
-		try {
-			conexao = ConnectionFactory.getConnection();
-			PreparedStatement stmt = conexao.prepareStatement(sql);
-			if (tipo == 1) {
-				stmt.setString(1, "%" + valor.toUpperCase() + "%");
-			}
-
-			ResultSet rs = stmt.executeQuery();
-
-			while (rs.next()) {
-				EscolaridadeBean p = new EscolaridadeBean();
-
-				p.setCodescolaridade(rs.getInt("id_escolaridade"));
-				p.setDescescolaridade(rs.getString("descescolaridade")
-						.toUpperCase());
-
-				lista.add(p);
-
-			}
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-			// throw new RuntimeException(ex); //
-		} finally {
-			try {
-				conexao.close();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				System.exit(1);
-			}
-		}
-		return lista;
-	}
+            }
+            return colecao;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                conexao.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
