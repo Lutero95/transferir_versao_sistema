88e72f35753e183c1b3a9d3923331d3920eb62f3
viniciusjlc <viniciusjlc@gmail.com>
2019-06-13 23:33:36 -0300

    - Adicionando campos de saúde no modelo do questionario e seus respectivos métodos get e set.

--- sishosp/src/br/gov/al/maceio/sishosp/questionario/model/Pestalozzi.java
+++ sishosp/src/br/gov/al/maceio/sishosp/questionario/model/Pestalozzi.java
@@ -3,11 +3,48 @@ package br.gov.al.maceio.sishosp.questionario.model;
 import java.io.Serializable;
 
 public class Pestalozzi implements Serializable {
-
     private Integer id;
-
-    // HERDADOS
-
+    //SAÚDE
+    private String saudeRealizaAtendimentoNestaInstituicao;
+    private String saudeUnidade;
+    private String saudeServicoAtendimentoRedeReabilitacaoAuditiva;
+    private String saudeServicoAtendimentoRedeReabilitacaoFisica;
+    private String saudeServicoAtendimentoRedeReabilitacaoIntelectual;
+    private String saudeServicoAtendimentoRedeReabilitacaoVisual;
+    private String saudeServicoAmbulatorio;
+    private String saudeOutrosServicosHomeCare;
+    private String saudeOutrosServicosEquoterapia;
+    private String saudeOutrosServicosGrupoConvivencia;
+    private String saudeOutrosServicosGrupoTerapeuticoFamiliar;
+    private String saudeOutrosServicosOrteseProtesesMeiosLocomocao;
+    private String saudeOutrosServicosOficinaOrtopedica;
+    private String saudeOutrosServicosEstimulacaoPrecoce;
+    private String saudeOutrosServicosHidroTerapia;
+    private String saudeUsoOrtesePrótese;
+    private String saudeUsoOrtesePróteseQuantoTempo;
+    private String saudeUsoOrtesePróteseQual;
+    private String saudeUsaEquipamentoOrtopedicoOrtese;
+    private String saudeUsaEquipamentoOrtopedicoAASI;
+    private String saudeUsaEquipamentoOrtopedicoAuxílioOptico;
+    private String saudeUsaEquipamentoOrtopedicoMeiosAuxiliaresDeLocomocao;
+    private String saudeUsaEquipamentoOrtopedicoProtese;
+    private String saudeUsaEquipamentoOrtopedicoOculosAdaptado;
+    private String saudeUsaEquipamentoOrtopedicoQuantoTempo;
+    private String saudeUsaEquipamentoOrtopedicoEntidadeQueConcedeu;
+    private String saudeUsaEquipamentoOrtopedicoRealizouReabilitacao;
+    private String saudeUsaEquipamentoOrtopedicoRealizouReabilitacaoOnde;
+    private String saudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficiencia;
+    private String saudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficienciaParentesco;
+    private String saudeUsaEquipamentoOrtopedicoMorbidadeCID;
+    private String saudeUsaEquipamentoOrtopedicoMorbidadeCIDQuantoTempo;
+    private String saudeUsaEquipamentoOrtopedicoCausaDoençaDeficiencia;
+    private String saudeUsaEquipamentoOrtopedicoResidenciaCobertaPSF;
+    private String saudeUsaEquipamentoOrtopedicoRecebeuVisitaAgenteSaude;
+    private String saudeUsaEquipamentoOrtopedicoFazParaSerAtendidoDoente;
+    private String saudeUsaEquipamentoOrtopedicoPostoSaudeRegiaoReside;
+    private String saudeUsaEquipamentoOrtopedicoUsoMedicacao;
+    private String saudeUsaEquipamentoOrtopedicoUsoMedicacaoQual;
+    private String saudeUsaEquipamentoOrtopedicoUsoMedicacaoFrequencia;
 
     public Pestalozzi() {
 
@@ -19,5 +56,325 @@ public class Pestalozzi implements Serializable {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getSaudeRealizaAtendimentoNestaInstituicao() {
+        return saudeRealizaAtendimentoNestaInstituicao;
+    }
+
+    public void setSaudeRealizaAtendimentoNestaInstituicao(String saudeRealizaAtendimentoNestaInstituicao) {
+        this.saudeRealizaAtendimentoNestaInstituicao = saudeRealizaAtendimentoNestaInstituicao;
+    }
+
+    public String getSaudeUnidade() {
+        return saudeUnidade;
+    }
+
+    public void setSaudeUnidade(String saudeUnidade) {
+        this.saudeUnidade = saudeUnidade;
+    }
+
+    public String getSaudeServicoAtendimentoRedeReabilitacaoAuditiva() {
+        return saudeServicoAtendimentoRedeReabilitacaoAuditiva;
+    }
+
+    public void setSaudeServicoAtendimentoRedeReabilitacaoAuditiva(String saudeServicoAtendimentoRedeReabilitacaoAuditiva) {
+        this.saudeServicoAtendimentoRedeReabilitacaoAuditiva = saudeServicoAtendimentoRedeReabilitacaoAuditiva;
+    }
+
+    public String getSaudeServicoAtendimentoRedeReabilitacaoFisica() {
+        return saudeServicoAtendimentoRedeReabilitacaoFisica;
+    }
+
+    public void setSaudeServicoAtendimentoRedeReabilitacaoFisica(String saudeServicoAtendimentoRedeReabilitacaoFisica) {
+        this.saudeServicoAtendimentoRedeReabilitacaoFisica = saudeServicoAtendimentoRedeReabilitacaoFisica;
+    }
+
+    public String getSaudeServicoAtendimentoRedeReabilitacaoIntelectual() {
+        return saudeServicoAtendimentoRedeReabilitacaoIntelectual;
+    }
+
+    public void setSaudeServicoAtendimentoRedeReabilitacaoIntelectual(String saudeServicoAtendimentoRedeReabilitacaoIntelectual) {
+        this.saudeServicoAtendimentoRedeReabilitacaoIntelectual = saudeServicoAtendimentoRedeReabilitacaoIntelectual;
+    }
+
+    public String getSaudeServicoAtendimentoRedeReabilitacaoVisual() {
+        return saudeServicoAtendimentoRedeReabilitacaoVisual;
+    }
+
+    public void setSaudeServicoAtendimentoRedeReabilitacaoVisual(String saudeServicoAtendimentoRedeReabilitacaoVisual) {
+        this.saudeServicoAtendimentoRedeReabilitacaoVisual = saudeServicoAtendimentoRedeReabilitacaoVisual;
+    }
+
+    public String getSaudeServicoAmbulatorio() {
+        return saudeServicoAmbulatorio;
+    }
+
+    public void setSaudeServicoAmbulatorio(String saudeServicoAmbulatorio) {
+        this.saudeServicoAmbulatorio = saudeServicoAmbulatorio;
+    }
+
+    public String getSaudeOutrosServicosHomeCare() {
+        return saudeOutrosServicosHomeCare;
+    }
+
+    public void setSaudeOutrosServicosHomeCare(String saudeOutrosServicosHomeCare) {
+        this.saudeOutrosServicosHomeCare = saudeOutrosServicosHomeCare;
+    }
+
+    public String getSaudeOutrosServicosEquoterapia() {
+        return saudeOutrosServicosEquoterapia;
+    }
+
+    public void setSaudeOutrosServicosEquoterapia(String saudeOutrosServicosEquoterapia) {
+        this.saudeOutrosServicosEquoterapia = saudeOutrosServicosEquoterapia;
+    }
+
+    public String getSaudeOutrosServicosGrupoConvivencia() {
+        return saudeOutrosServicosGrupoConvivencia;
+    }
+
+    public void setSaudeOutrosServicosGrupoConvivencia(String saudeOutrosServicosGrupoConvivencia) {
+        this.saudeOutrosServicosGrupoConvivencia = saudeOutrosServicosGrupoConvivencia;
+    }
+
+    public String getSaudeOutrosServicosGrupoTerapeuticoFamiliar() {
+        return saudeOutrosServicosGrupoTerapeuticoFamiliar;
+    }
+
+    public void setSaudeOutrosServicosGrupoTerapeuticoFamiliar(String saudeOutrosServicosGrupoTerapeuticoFamiliar) {
+        this.saudeOutrosServicosGrupoTerapeuticoFamiliar = saudeOutrosServicosGrupoTerapeuticoFamiliar;
+    }
+
+    public String getSaudeOutrosServicosOrteseProtesesMeiosLocomocao() {
+        return saudeOutrosServicosOrteseProtesesMeiosLocomocao;
+    }
+
+    public void setSaudeOutrosServicosOrteseProtesesMeiosLocomocao(String saudeOutrosServicosOrteseProtesesMeiosLocomocao) {
+        this.saudeOutrosServicosOrteseProtesesMeiosLocomocao = saudeOutrosServicosOrteseProtesesMeiosLocomocao;
+    }
+
+    public String getSaudeOutrosServicosOficinaOrtopedica() {
+        return saudeOutrosServicosOficinaOrtopedica;
+    }
+
+    public void setSaudeOutrosServicosOficinaOrtopedica(String saudeOutrosServicosOficinaOrtopedica) {
+        this.saudeOutrosServicosOficinaOrtopedica = saudeOutrosServicosOficinaOrtopedica;
+    }
+
+    public String getSaudeOutrosServicosEstimulacaoPrecoce() {
+        return saudeOutrosServicosEstimulacaoPrecoce;
+    }
+
+    public void setSaudeOutrosServicosEstimulacaoPrecoce(String saudeOutrosServicosEstimulacaoPrecoce) {
+        this.saudeOutrosServicosEstimulacaoPrecoce = saudeOutrosServicosEstimulacaoPrecoce;
+    }
+
+    public String getSaudeOutrosServicosHidroTerapia() {
+        return saudeOutrosServicosHidroTerapia;
+    }
+
+    public void setSaudeOutrosServicosHidroTerapia(String saudeOutrosServicosHidroTerapia) {
+        this.saudeOutrosServicosHidroTerapia = saudeOutrosServicosHidroTerapia;
+    }
+
+    public String getSaudeUsoOrtesePrótese() {
+        return saudeUsoOrtesePrótese;
+    }
+
+    public void setSaudeUsoOrtesePrótese(String saudeUsoOrtesePrótese) {
+        this.saudeUsoOrtesePrótese = saudeUsoOrtesePrótese;
+    }
+
+    public String getSaudeUsoOrtesePróteseQuantoTempo() {
+        return saudeUsoOrtesePróteseQuantoTempo;
+    }
+
+    public void setSaudeUsoOrtesePróteseQuantoTempo(String saudeUsoOrtesePróteseQuantoTempo) {
+        this.saudeUsoOrtesePróteseQuantoTempo = saudeUsoOrtesePróteseQuantoTempo;
+    }
+
+    public String getSaudeUsoOrtesePróteseQual() {
+        return saudeUsoOrtesePróteseQual;
+    }
+
+    public void setSaudeUsoOrtesePróteseQual(String saudeUsoOrtesePróteseQual) {
+        this.saudeUsoOrtesePróteseQual = saudeUsoOrtesePróteseQual;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoOrtese() {
+        return saudeUsaEquipamentoOrtopedicoOrtese;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoOrtese(String saudeUsaEquipamentoOrtopedicoOrtese) {
+        this.saudeUsaEquipamentoOrtopedicoOrtese = saudeUsaEquipamentoOrtopedicoOrtese;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoAASI() {
+        return saudeUsaEquipamentoOrtopedicoAASI;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoAASI(String saudeUsaEquipamentoOrtopedicoAASI) {
+        this.saudeUsaEquipamentoOrtopedicoAASI = saudeUsaEquipamentoOrtopedicoAASI;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoAuxílioOptico() {
+        return saudeUsaEquipamentoOrtopedicoAuxílioOptico;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoAuxílioOptico(String saudeUsaEquipamentoOrtopedicoAuxílioOptico) {
+        this.saudeUsaEquipamentoOrtopedicoAuxílioOptico = saudeUsaEquipamentoOrtopedicoAuxílioOptico;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoMeiosAuxiliaresDeLocomocao() {
+        return saudeUsaEquipamentoOrtopedicoMeiosAuxiliaresDeLocomocao;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoMeiosAuxiliaresDeLocomocao(String saudeUsaEquipamentoOrtopedicoMeiosAuxiliaresDeLocomocao) {
+        this.saudeUsaEquipamentoOrtopedicoMeiosAuxiliaresDeLocomocao = saudeUsaEquipamentoOrtopedicoMeiosAuxiliaresDeLocomocao;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoProtese() {
+        return saudeUsaEquipamentoOrtopedicoProtese;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoProtese(String saudeUsaEquipamentoOrtopedicoProtese) {
+        this.saudeUsaEquipamentoOrtopedicoProtese = saudeUsaEquipamentoOrtopedicoProtese;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoOculosAdaptado() {
+        return saudeUsaEquipamentoOrtopedicoOculosAdaptado;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoOculosAdaptado(String saudeUsaEquipamentoOrtopedicoOculosAdaptado) {
+        this.saudeUsaEquipamentoOrtopedicoOculosAdaptado = saudeUsaEquipamentoOrtopedicoOculosAdaptado;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoQuantoTempo() {
+        return saudeUsaEquipamentoOrtopedicoQuantoTempo;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoQuantoTempo(String saudeUsaEquipamentoOrtopedicoQuantoTempo) {
+        this.saudeUsaEquipamentoOrtopedicoQuantoTempo = saudeUsaEquipamentoOrtopedicoQuantoTempo;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoEntidadeQueConcedeu() {
+        return saudeUsaEquipamentoOrtopedicoEntidadeQueConcedeu;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoEntidadeQueConcedeu(String saudeUsaEquipamentoOrtopedicoEntidadeQueConcedeu) {
+        this.saudeUsaEquipamentoOrtopedicoEntidadeQueConcedeu = saudeUsaEquipamentoOrtopedicoEntidadeQueConcedeu;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoRealizouReabilitacao() {
+        return saudeUsaEquipamentoOrtopedicoRealizouReabilitacao;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoRealizouReabilitacao(String saudeUsaEquipamentoOrtopedicoRealizouReabilitacao) {
+        this.saudeUsaEquipamentoOrtopedicoRealizouReabilitacao = saudeUsaEquipamentoOrtopedicoRealizouReabilitacao;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoRealizouReabilitacaoOnde() {
+        return saudeUsaEquipamentoOrtopedicoRealizouReabilitacaoOnde;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoRealizouReabilitacaoOnde(String saudeUsaEquipamentoOrtopedicoRealizouReabilitacaoOnde) {
+        this.saudeUsaEquipamentoOrtopedicoRealizouReabilitacaoOnde = saudeUsaEquipamentoOrtopedicoRealizouReabilitacaoOnde;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficiencia() {
+        return saudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficiencia;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficiencia(String saudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficiencia) {
+        this.saudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficiencia = saudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficiencia;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficienciaParentesco() {
+        return saudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficienciaParentesco;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficienciaParentesco(String saudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficienciaParentesco) {
+        this.saudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficienciaParentesco = saudeUsaEquipamentoOrtopedicoAlguemNaFamiliaComDeficienciaParentesco;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoMorbidadeCID() {
+        return saudeUsaEquipamentoOrtopedicoMorbidadeCID;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoMorbidadeCID(String saudeUsaEquipamentoOrtopedicoMorbidadeCID) {
+        this.saudeUsaEquipamentoOrtopedicoMorbidadeCID = saudeUsaEquipamentoOrtopedicoMorbidadeCID;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoMorbidadeCIDQuantoTempo() {
+        return saudeUsaEquipamentoOrtopedicoMorbidadeCIDQuantoTempo;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoMorbidadeCIDQuantoTempo(String saudeUsaEquipamentoOrtopedicoMorbidadeCIDQuantoTempo) {
+        this.saudeUsaEquipamentoOrtopedicoMorbidadeCIDQuantoTempo = saudeUsaEquipamentoOrtopedicoMorbidadeCIDQuantoTempo;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoCausaDoençaDeficiencia() {
+        return saudeUsaEquipamentoOrtopedicoCausaDoençaDeficiencia;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoCausaDoençaDeficiencia(String saudeUsaEquipamentoOrtopedicoCausaDoençaDeficiencia) {
+        this.saudeUsaEquipamentoOrtopedicoCausaDoençaDeficiencia = saudeUsaEquipamentoOrtopedicoCausaDoençaDeficiencia;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoResidenciaCobertaPSF() {
+        return saudeUsaEquipamentoOrtopedicoResidenciaCobertaPSF;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoResidenciaCobertaPSF(String saudeUsaEquipamentoOrtopedicoResidenciaCobertaPSF) {
+        this.saudeUsaEquipamentoOrtopedicoResidenciaCobertaPSF = saudeUsaEquipamentoOrtopedicoResidenciaCobertaPSF;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoRecebeuVisitaAgenteSaude() {
+        return saudeUsaEquipamentoOrtopedicoRecebeuVisitaAgenteSaude;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoRecebeuVisitaAgenteSaude(String saudeUsaEquipamentoOrtopedicoRecebeuVisitaAgenteSaude) {
+        this.saudeUsaEquipamentoOrtopedicoRecebeuVisitaAgenteSaude = saudeUsaEquipamentoOrtopedicoRecebeuVisitaAgenteSaude;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoFazParaSerAtendidoDoente() {
+        return saudeUsaEquipamentoOrtopedicoFazParaSerAtendidoDoente;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoFazParaSerAtendidoDoente(String saudeUsaEquipamentoOrtopedicoFazParaSerAtendidoDoente) {
+        this.saudeUsaEquipamentoOrtopedicoFazParaSerAtendidoDoente = saudeUsaEquipamentoOrtopedicoFazParaSerAtendidoDoente;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoPostoSaudeRegiaoReside() {
+        return saudeUsaEquipamentoOrtopedicoPostoSaudeRegiaoReside;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoPostoSaudeRegiaoReside(String saudeUsaEquipamentoOrtopedicoPostoSaudeRegiaoReside) {
+        this.saudeUsaEquipamentoOrtopedicoPostoSaudeRegiaoReside = saudeUsaEquipamentoOrtopedicoPostoSaudeRegiaoReside;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoUsoMedicacao() {
+        return saudeUsaEquipamentoOrtopedicoUsoMedicacao;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoUsoMedicacao(String saudeUsaEquipamentoOrtopedicoUsoMedicacao) {
+        this.saudeUsaEquipamentoOrtopedicoUsoMedicacao = saudeUsaEquipamentoOrtopedicoUsoMedicacao;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoUsoMedicacaoQual() {
+        return saudeUsaEquipamentoOrtopedicoUsoMedicacaoQual;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoUsoMedicacaoQual(String saudeUsaEquipamentoOrtopedicoUsoMedicacaoQual) {
+        this.saudeUsaEquipamentoOrtopedicoUsoMedicacaoQual = saudeUsaEquipamentoOrtopedicoUsoMedicacaoQual;
+    }
+
+    public String getSaudeUsaEquipamentoOrtopedicoUsoMedicacaoFrequencia() {
+        return saudeUsaEquipamentoOrtopedicoUsoMedicacaoFrequencia;
+    }
+
+    public void setSaudeUsaEquipamentoOrtopedicoUsoMedicacaoFrequencia(String saudeUsaEquipamentoOrtopedicoUsoMedicacaoFrequencia) {
+        this.saudeUsaEquipamentoOrtopedicoUsoMedicacaoFrequencia = saudeUsaEquipamentoOrtopedicoUsoMedicacaoFrequencia;
     }
 }
