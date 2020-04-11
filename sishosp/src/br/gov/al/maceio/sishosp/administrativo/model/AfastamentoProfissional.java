ef8423fda1e37bd3047c153929d4d705fca0fbc3
Walter <walter.junior@dti.maceio.al.gov.br>
2020-04-11 15:08:57 -0300

    ajuste no texto que nao estava UTF-8

--- sishosp/src/br/gov/al/maceio/sishosp/administrativo/model/AfastamentoProfissional.java
+++ sishosp/src/br/gov/al/maceio/sishosp/administrativo/model/AfastamentoProfissional.java
@@ -19,7 +19,7 @@ public class AfastamentoProfissional implements Serializable {
 
     public AfastamentoProfissional() {
         funcionario = new FuncionarioBean();
-        tipoAfastamento = TipoAfastamento.TEMPORÃ.getSigla();
+        tipoAfastamento = TipoAfastamento.TEMPOR.getSigla();
     }
 
     public Integer getId() {
