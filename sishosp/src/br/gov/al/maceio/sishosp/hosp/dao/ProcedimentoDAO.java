789cca10788515fa0d829944f6521604aeab1ef5
Walter <walter.junior@dti.maceio.al.gov.br>
2020-04-25 21:49:59 -0300

    ajustes na rotina de carga sigtap para resolver carga do procedimento de terapia em grupo (em andamento)

--- sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/ProcedimentoDAO.java
+++ sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/ProcedimentoDAO.java
@@ -1077,14 +1077,25 @@ public class ProcedimentoDAO {
 			}
 			stm.setString(7, procedimentoMensalDTO.getProcedimentoMensal().getSexoPermitido());
 			
-			if(VerificadorUtil.verificarSeObjetoNulo(procedimentoMensalDTO.getProcedimentoMensal().getQuantidadeMaxima()))
+			if(VerificadorUtil.verificarSeObjetoNuloOuZero(procedimentoMensalDTO.getProcedimentoMensal().getQuantidadeMaxima()))
 				stm.setNull(8, Types.NULL);
 			else	
 				stm.setInt(8, procedimentoMensalDTO.getProcedimentoMensal().getQuantidadeMaxima());
 			
-			stm.setInt(9, procedimentoMensalDTO.getProcedimentoMensal().getIdadeMinimaPermitida().getQuantidadeLimite());
+			if ( (procedimentoMensalDTO!=null) || (procedimentoMensalDTO.getProcedimentoMensal()!=null) || (procedimentoMensalDTO.getProcedimentoMensal().getIdadeMinimaPermitida()!=null) || (procedimentoMensalDTO.getProcedimentoMensal().getIdadeMinimaPermitida().getQuantidadeLimite()!=null))
+				stm.setNull(9, Types.NULL);
+			else	
+				stm.setInt(9, procedimentoMensalDTO.getProcedimentoMensal().getIdadeMinimaPermitida().getQuantidadeLimite());
+			
+
 			stm.setString(10, procedimentoMensalDTO.getProcedimentoMensal().getIdadeMinimaPermitida().getUnidadeLimite().name());
-			stm.setInt(11, procedimentoMensalDTO.getProcedimentoMensal().getIdadeMaximaPermitida().getQuantidadeLimite());
+			
+			if ((VerificadorUtil.verificarSeObjetoNuloOuZero(procedimentoMensalDTO.getProcedimentoMensal().getIdadeMaximaPermitida())) || (VerificadorUtil.verificarSeObjetoNuloOuZero(procedimentoMensalDTO.getProcedimentoMensal().getIdadeMaximaPermitida().getQuantidadeLimite())))
+				stm.setNull(11, Types.NULL);
+			else	
+				stm.setInt(11, procedimentoMensalDTO.getProcedimentoMensal().getIdadeMaximaPermitida().getQuantidadeLimite());
+			
+			
 			stm.setString(12, procedimentoMensalDTO.getProcedimentoMensal().getIdadeMaximaPermitida().getUnidadeLimite().name());
 			stm.setDouble(13, procedimentoMensalDTO.getProcedimentoMensal().getValorSA().doubleValue());
 			stm.setDouble(14, procedimentoMensalDTO.getProcedimentoMensal().getValorSH().doubleValue());
@@ -1103,6 +1114,10 @@ public class ProcedimentoDAO {
 			if(rs.next())
 				idProcedimentoMensal = rs.getInt("id");
 		} catch (Exception ex) {
+			System.out.println("(procedimentoMensalDTO"+procedimentoMensalDTO);
+			System.out.println("(procedimentoMensalDTO.getProcedimentoMensal()"+procedimentoMensalDTO.getProcedimentoMensal());
+System.out.println("(procedimentoMensalDTO.getProcedimentoMensal().getIdadeMinimaPermitida()"+procedimentoMensalDTO.getProcedimentoMensal().getIdadeMinimaPermitida());
+System.out.println("(procedimentoMensalDTO.getProcedimentoMensal().getIdadeMinimaPermitida().getQuantidadeLimite()"+procedimentoMensalDTO.getProcedimentoMensal().getIdadeMinimaPermitida().getQuantidadeLimite());
 			conexao.rollback();
 			ex.printStackTrace();
 			throw new ProjetoException(ex);
