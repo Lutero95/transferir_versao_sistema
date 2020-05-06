db7d3f82a304571680cea8d49c395a73bc0b2a78
Manoel Henrique <henriqe115@gmail.com>
2020-05-05 21:01:48 -0300

    alterações no model de endereco e criação do model de logradouro

---
+++ sishosp/src/br/gov/al/maceio/sishosp/hosp/model/LogradouroBean.java
@@ -0,0 +1,28 @@
+package br.gov.al.maceio.sishosp.hosp.model;
+
+public class LogradouroBean {
+
+	private Integer id;
+	private String codigo;
+	private String logradouro;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+}
