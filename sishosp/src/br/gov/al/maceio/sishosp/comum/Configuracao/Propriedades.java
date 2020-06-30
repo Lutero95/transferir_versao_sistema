e7e9be9a281e829d15dfdd10b096b6e88ab9afb3
Walter <walter.junior@dti.maceio.al.gov.br>
2020-06-30 17:17:52 -0300

    ajuste no sql do relatorio para pegar os dados da situacao de atendimento da tabela situacao_atendimento atendimentosporprogramagrupo.jrxml q

--- sishosp/src/br/gov/al/maceio/sishosp/comum/Configuracao/Propriedades.java
+++ sishosp/src/br/gov/al/maceio/sishosp/comum/Configuracao/Propriedades.java
@@ -2,7 +2,7 @@ package br.gov.al.maceio.sishosp.comum.Configuracao;
 
 public class Propriedades {
 
-    public static Conexoes Conexao = Conexoes.LOCALHOST ;
+    public static Conexoes Conexao = Conexoes.PRODUCAO ;
 
     public enum Conexoes {
         LOCALHOST,
