b7cf0cebc5a95db08866610d6d23f313ca9dc2c8
Walter <walter.junior@dti.maceio.al.gov.br>
2020-11-11 15:27:45 -0300

    ajuste no sql queetava excluindo atendimentos com evolucao

--- sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/GerenciarPacienteDAO.java
+++ sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/GerenciarPacienteDAO.java
@@ -468,7 +468,7 @@ public class GerenciarPacienteDAO {
                     "LEFT JOIN hosp.atendimentos a ON (a.id_atendimento = a1.id_atendimento) " +
                     "WHERE   coalesce(a1.excluido, 'N' )='N' and  a.id_paciente_instituicao = ? AND a.dtaatende >= current_date AND  " +
                     "(SELECT count(*) FROM hosp.atendimentos1 aa1 WHERE aa1.id_atendimento = a1.id_atendimento and coalesce(aa1.excluido, 'N' )='N') = " +
-                    "(SELECT count(*) FROM hosp.atendimentos1 aaa1 WHERE aaa1.id_atendimento = a1.id_atendimento AND a1.id_situacao_atendimento IS NULL and coalesce(aaa1.excluido, 'N' )='N') " +
+                    "(SELECT count(*) FROM hosp.atendimentos1 aaa1 WHERE aaa1.id_atendimento = a1.id_atendimento AND aaa1.id_situacao_atendimento IS NULL and coalesce(aaa1.excluido, 'N' )='N') " +
                     "ORDER BY a1.id_atendimento;";
 
 
