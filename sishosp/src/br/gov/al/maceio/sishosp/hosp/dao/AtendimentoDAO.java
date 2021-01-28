610959d6ca0f7c2240be450dbcf120662e16e77b
Walter <walter.junior@dti.maceio.al.gov.br>
2021-01-27 21:47:38 -0300

    correcao na duplicidade de pacientes na tela de evolucao do profissional

--- sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/AtendimentoDAO.java
+++ sishosp/src/br/gov/al/maceio/sishosp/hosp/dao/AtendimentoDAO.java
@@ -770,7 +770,7 @@ public class AtendimentoDAO {
 		FuncionarioBean user_session = (FuncionarioBean) FacesContext.getCurrentInstance().getExternalContext()
 				.getSessionMap().get("obj_funcionario");
 
-		String sql = "select distinct  coalesce(a.presenca,'N') presenca, a.id_atendimento, a1.id_atendimentos1, a.dtaatende, a.codpaciente, p.nome, p.cns,p.cpf,  a.turno,a1.codprofissionalatendimento, f.descfuncionario,"
+		String sql = "select distinct  coalesce(a.presenca,'N') presenca, a.id_atendimento,  a.dtaatende, a.codpaciente, p.nome, p.cns,p.cpf,  a.turno,a1.codprofissionalatendimento, f.descfuncionario,"
 				+ " a.codprograma, pr.descprograma, a.codtipoatendimento, t.desctipoatendimento,"
 				+ " a.codequipe, e.descequipe, a.codgrupo, g.descgrupo, a.avaliacao, parm.bloqueia_por_pendencia_evolucao_anterior, "
 				+ " case when t.equipe_programa is true then 'Sim' else 'Não' end as ehEquipe, a.cod_unidade, a1.id_situacao_atendimento, sa.abono_falta, sa.descricao descsitatendimento,  "
@@ -910,7 +910,7 @@ public class AtendimentoDAO {
 				AtendimentoBean atendimentoBean = new AtendimentoBean();
 				atendimentoBean.setPresenca(rs.getString("presenca"));
 				atendimentoBean.setId(rs.getInt("id_atendimento"));
-				atendimentoBean.setId1(rs.getInt("id_atendimentos1"));
+				//atendimentoBean.setId1(rs.getInt("id_atendimentos1"));
 				atendimentoBean.setDataAtendimentoInicio(rs.getDate("dtaatende"));
 				atendimentoBean.getPaciente().setId_paciente(rs.getInt("codpaciente"));
 				atendimentoBean.getPaciente().setNome(rs.getString("nome"));
