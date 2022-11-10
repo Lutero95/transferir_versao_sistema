bd2315914492c51dd411ea89d69585570a0f2fff
Taígo Pedrosa <timp@ic.ufal.br>
2022-11-09 21:07:00 -0300

    Removes cns already exists check. Should not be a part of the *validator* (#13)
    Co-authored-by: TaigoI <taigo.pedrosa@gmail.com>

--- sishosp/src/br/gov/al/maceio/sishosp/comum/validator/CNSProfissionalValidator.java
+++ sishosp/src/br/gov/al/maceio/sishosp/comum/validator/CNSProfissionalValidator.java
@@ -1,8 +1,5 @@
 package br.gov.al.maceio.sishosp.comum.validator;
 
-import br.gov.al.maceio.sishosp.acl.dao.FuncionarioDAO;
-import br.gov.al.maceio.sishosp.acl.model.FuncionarioBean;
-import br.gov.al.maceio.sishosp.comum.exception.ProjetoException;
 import br.gov.al.maceio.sishosp.comum.util.DocumentosUtil;
 import br.gov.al.maceio.sishosp.comum.util.VerificadorUtil;
 
@@ -27,19 +24,6 @@ public class CNSProfissionalValidator implements Validator {
 				message.setSummary("CNS Inválido!");
 				throw new ValidatorException(message);
 			}
-
-			FuncionarioDAO fdao = new FuncionarioDAO();
-			try {
-				FuncionarioBean retorno = fdao.verificaExisteCnsCadastrado(valorTelaString);
-				if (retorno != null) {
-					FacesMessage message = new FacesMessage();
-					message.setSeverity(FacesMessage.SEVERITY_ERROR);
-					message.setSummary("O CNS informado já está cadastrado para " + retorno.getNome());
-					throw new ValidatorException(message);
-				}
-			} catch (ProjetoException e) {
-				//comentado walter erro log ex.printStackTrace();
-			} 
 		}
 	}
 
